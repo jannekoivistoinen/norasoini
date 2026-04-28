a430467604c646213037f320ddc916fd4cefbe60
Janne Koivistoinen <janne@sharetribe.com>
2026-04-28 17:40:05 +0300

    optim

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const config: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  experimental: {
+    inlineCss: true,
+  },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
